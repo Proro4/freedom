f8925601d8c469d7d5d92d6719fdac2ad0552bc5
DenisDev <proro4@gmail.com>
2018-06-20 08:02:10 +0300

    create dekstop version

---
+++ app/scripts/main.js
@@ -0,0 +1,39 @@
+$("[data-fancybox]").fancybox({ 
+	buttons : [
+	'close']
+	});
+
+$('.info-4__row__item').on('click',function(){   
+$('button.fancybox-button.fancybox-button--close').remove();
+$('.fancybox-navigation').remove(); 
+			$('.fancybox-content').append('<button data-fancybox-close="" class="fancybox-button fancybox-button--close" title="Close"></path></svg></button>');
+			$('.fancybox-content').append('<div class="fancybox-navigation"><a data-fancybox-prev="" class="fancybox-button fancybox-button--arrow_left" title="Previous" href="javascript:;"><svg viewBox="0 0 40 40"><path d="M18,12 L10,20 L18,28 M10,20 L30,20"></path></svg></a><a data-fancybox-next="" class="fancybox-button fancybox-button--arrow_right disabled" title="Next" href="javascript:;"><svg viewBox="0 0 40 40"><path d="M10,20 L30,20 M22,12 L30,20 L22,28"></path></svg></a></div>'); 
+			$('.fancybox-content').each(function(){
+				console.log($(this).length);
+			})
+function fancyClick(){
+$('.fancybox-content').on('click',function(){
+	setTimeout(function(){ 
+		console.log('click');
+			$('.fancybox-slide.fancybox-slide--image .fancybox-content').append('<button data-fancybox-close="" class="fancybox-button fancybox-button--close" title="Close"></path></svg></button>');
+			$('.fancybox-slide.fancybox-slide--image .fancybox-content').append('<div class="fancybox-navigation"><a data-fancybox-prev="" class="fancybox-button fancybox-button--arrow_left" title="Previous" href="javascript:;"><svg viewBox="0 0 40 40"><path d="M18,12 L10,20 L18,28 M10,20 L30,20"></path></svg></a><a data-fancybox-next="" class="fancybox-button fancybox-button--arrow_right disabled" title="Next" href="javascript:;"><svg viewBox="0 0 40 40"><path d="M10,20 L30,20 M22,12 L30,20 L22,28"></path></svg></a></div>'); 
+	},500); 
+	fancyClick();
+})
+}
+fancyClick()
+})
+$(document).ready(function(){
+	setTimeout(function(){ 
+$('button.fancybox-button.fancybox-button--close').remove();
+$('.fancybox-navigation').remove(); 
+			$('.fancybox-slide.fancybox-slide--image .fancybox-content').append('<button data-fancybox-close="" class="fancybox-button fancybox-button--close" title="Close"></path></svg></button>');
+			$('.fancybox-slide.fancybox-slide--image .fancybox-content').append('<div class="fancybox-navigation"><a data-fancybox-prev="" class="fancybox-button fancybox-button--arrow_left" title="Previous" href="javascript:;"><svg viewBox="0 0 40 40"><path d="M18,12 L10,20 L18,28 M10,20 L30,20"></path></svg></a><a data-fancybox-next="" class="fancybox-button fancybox-button--arrow_right disabled" title="Next" href="javascript:;"><svg viewBox="0 0 40 40"><path d="M10,20 L30,20 M22,12 L30,20 L22,28"></path></svg></a></div>'); 
+
+fancyClick();
+		
+	},300); 
+	$('.fancybox-slide.fancybox-slide--image .fancybox-content').on('click',function(){
+		console.log('1');
+	})
+})
